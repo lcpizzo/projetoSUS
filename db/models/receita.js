31df6556d62f0f85f771a1c405dfc5e4a2e788d0
LuisFelipeJorge <46935669+LuisFelipeJorge@users.noreply.github.com>
2023-04-11 22:03:03 -0300

    Modelo objeto receita

---
+++ db/models/receita.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const receituarioModel = new mongoose.Schema({
+    medicamento: {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'medicamento',
+        required:true
+    },
+    frequencia: {
+        type: Int32Array,
+        required:true,
+    },
+    dose: {
+        type: String,
+        required:true,
+    }
+})
+
+const receitaSchema = new mongoose.Schema({
+    cod_receita:{
+        type:String,
+        required: true,
+        index:true,
+    },
+    receituario:{
+        type: [receituarioModel],
+        default: () => ({})
+    },
+    validade:{
+        type: Date,
+        required: true,
+    },
+    medico:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'funcionario',
+        required: true,
+    },
+    paciente:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'paciente',
+        required: true,
+    }
+})
+
+const receita = mongoose.model("Receita", receitaSchema);
+
+export default receita;
