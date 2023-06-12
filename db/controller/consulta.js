f860731851723058a6aa1bd1b663f1fce1fcbf40
guijun13 <guilhermejun13.grillo@gmail.com>
2023-06-11 21:52:49 -0300

    minor fixes

--- db/controller/consulta.js
+++ db/controller/consulta.js
@@ -4,21 +4,21 @@ try {
   await client.connect();
   await createMultipleAppointments(client, [
     {
-        codigo: {
-            medico:'muzzy',
-            paciente : 'jorgin',
-            dataConsulta: '10/06/2023'
-        },
-        dadosReceita: ['trembo 1.5mg por semana', 'deca 0.5mg por semana' ]
+      codigo: {
+        medico: 'muzzy',
+        paciente: 'jorgin',
+        dataConsulta: '10/06/2023',
+      },
+      dadosReceita: ['trembo 1.5mg por semana', 'deca 0.5mg por semana'],
     },
     {
-        codigo: {
-            medico:'pazuelo',
-            paciente : 'jun',
-            dataConsulta: '11/06/2023'
-        },
-        dadosReceita: ['cloroquina', 'ivermectina']
-    }
+      codigo: {
+        medico: 'pazuelo',
+        paciente: 'jun',
+        dataConsulta: '11/06/2023',
+      },
+      dadosReceita: ['cloroquina', 'ivermectina'],
+    },
   ]);
 } catch (e) {
   console.error(e);
@@ -26,35 +26,24 @@ try {
 
 // cria multiplos objetos consulta
 async function createMultipleAppointments(client, newAppointments) {
-  const result = await client
-  .db('test')
-  .collection('consultas')
-  .insertMany(newAppointments);
+  const result = await client.db('test').collection('consultas').insertMany(newAppointments);
 
   console.log(`${result.insertedCount} new appointments(s) created with the following id(s):`);
   console.log(result.insertedIds);
 }
 
 // retorna consulta buscada pela tupla (medico, paciente, data)
-async function findOneAppointmentByTuple(client,
-    {
-        doctor, 
-        patient, 
-        date
-    }={}
-) {
-  // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
-  const result = await client
-    .db('test')
-    .collection('consultas')
-    .findOne({ 
-        'codigo.medico': doctor,
-        'codigo.paciente': patient,
-        'codigo.dataConsulta':date
-     });
+async function findOneAppointmentByTuple(client, { doctor, patient, date } = {}) {
+  const result = await client.db('test').collection('consultas').findOne({
+    'codigo.medico': doctor,
+    'codigo.paciente': patient,
+    'codigo.dataConsulta': date,
+  });
 
   if (result) {
-    console.log(`Found an appointment in the collection with the tuple ('${doctor}', '${patient}', '${date}'):`);
+    console.log(
+      `Found an appointment in the collection with the tuple ('${doctor}', '${patient}', '${date}'):`
+    );
     console.log(result);
   } else {
     console.log(`No appointment found with the tuple ('${doctor}', '${patient}', '${date}'):`);
@@ -63,73 +52,55 @@ async function findOneAppointmentByTuple(client,
 
 // retorna todas as consultas de um determinado paciente
 async function findByPatient(client, patient) {
-  const cursor = client
-    .db('test')
-    .collection('consultas')
-    .find({
-        'codigo.paciente': patient
-    });
+  const cursor = client.db('test').collection('consultas').find({
+    'codigo.paciente': patient,
+  });
 
   // Store the results in an array
   const results = await cursor.toArray();
 
   // Print the results
   if (results.length > 0) {
-      console.log(`Found appointment(s) with patient ${patient}:`);
-      results.forEach((result, i) => {
-          const date = new Date(result.codigo.dataConsulta).toDateString();
+    console.log(`Found appointment(s) with patient ${patient}:`);
+    results.forEach((result, i) => {
+      const date = new Date(result.codigo.dataConsulta).toDateString();
 
-          console.log();
-          console.log(`${i + 1}. medico: ${result.codigo.medico}`);
-          console.log(`   _id: ${result._id}`);
-          console.log(`   paciente: ${result.codigo.paciente}`);
-          console.log(`   data: ${date}`);
-      });
+      console.log();
+      console.log(`${i + 1}. medico: ${result.codigo.medico}`);
+      console.log(`   _id: ${result._id}`);
+      console.log(`   paciente: ${result.codigo.paciente}`);
+      console.log(`   data: ${date}`);
+    });
   } else {
-      console.log(`No appointments found with patient ${patient}:`);
+    console.log(`No appointments found with patient ${patient}:`);
   }
 }
 
 //atualizar uma determinada consulta
-async function updateAppointmentByTuple(client,
+async function updateAppointmentByTuple(client, { doctor, patient, date } = {}, newAppointment) {
+  const result = await client.db('test').collection('consultas').updateOne(
     {
-        doctor, 
-        patient, 
-        date
-    }={},
-    newAppointment
-) {
-  // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
-  const result = await client
-    .db('test')
-    .collection('consultas')
-    .updateOne(
-        { 
-            'codigo.medico': doctor,
-            'codigo.paciente': patient,
-            'codigo.dataConsulta':date
-        },
-        {$set: newAppointment});
+      'codigo.medico': doctor,
+      'codigo.paciente': patient,
+      'codigo.dataConsulta': date,
+    },
+    { $set: newAppointment }
+  );
 
   if (result) {
     console.log(`${result.matchedCount} document(s) matched the query criteria.`);
-    console.log(`${result.modifiedCount} document(s) was/were updated.`);;
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
   } else {
     console.log(`No appointment found with the tuple ('${doctor}', '${patient}', '${date}'):`);
   }
 }
 
-
 // excluir uma consulta especifica
 async function deleteAppointmentByTuple(client, doctor, patient, date) {
-    const result = await client
-    .db("test")
-    .collection("consultas")
-    .deleteOne({ 
-        'codigo.medico': doctor,
-        'codigo.paciente': patient,
-        'codigo.dataConsulta':date
-    });
-    console.log(`${result.deletedCount} document(s) was/were deleted.`);
+  const result = await client.db('test').collection('consultas').deleteOne({
+    'codigo.medico': doctor,
+    'codigo.paciente': patient,
+    'codigo.dataConsulta': date,
+  });
+  console.log(`${result.deletedCount} document(s) was/were deleted.`);
 }
-
--- db/controller/receita.js
+++ db/controller/receita.js
@@ -5,27 +5,27 @@ try {
   await client.connect();
   await createMultiplePrescriptions(client, [
     {
-        cod_receita: '123',
-        receituario: {
-            medicamento:{ nome: 'dipirona', codigo: '123456', preco: 25.48},
-            frequencia : 2,
-            dose: 'comprimido 500 ml'
-        },
-        validade: '10/06/2024',
-        medico: 'muzzi',
-        paciente: 'jorge'
+      cod_receita: '123',
+      receituario: {
+        medicamento: { nome: 'dipirona', codigo: '123456', preco: 25.48 },
+        frequencia: 2,
+        dose: 'comprimido 500 ml',
+      },
+      validade: '10/06/2024',
+      medico: 'muzzi',
+      paciente: 'jorge',
     },
     {
-        cod_receita: '123456',
-        receituario: {
-            medicamento:{ nome: 'rivotril', codigo: '12356', preco: 25.48},
-            frequencia : 2,
-            dose: 'comprimido 500 ml'
-        },
-        validade: '10/07/2024',
-        medico: 'pazuelo',
-        paciente: 'jun'
-    }
+      cod_receita: '123456',
+      receituario: {
+        medicamento: { nome: 'rivotril', codigo: '12356', preco: 25.48 },
+        frequencia: 2,
+        dose: 'comprimido 500 ml',
+      },
+      validade: '10/07/2024',
+      medico: 'pazuelo',
+      paciente: 'jun',
+    },
   ]);
 } catch (e) {
   console.error(e);
@@ -33,10 +33,7 @@ try {
 
 // cria multiplos objetos receita
 async function createMultiplePrescriptions(client, newPrescriptions) {
-  const result = await client
-  .db('test')
-  .collection('receitas')
-  .insertMany(newPrescriptions);
+  const result = await client.db('test').collection('receitas').insertMany(newPrescriptions);
 
   console.log(`${result.insertedCount} new prescription(s) created with the following id(s):`);
   console.log(result.insertedIds);
@@ -44,13 +41,9 @@ async function createMultiplePrescriptions(client, newPrescriptions) {
 
 // retorna consulta buscada pelo codigo da receita
 async function findOnePrescriptionByCod(client, cod_receita_alvo) {
-  // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
-  const result = await client
-    .db('test')
-    .collection('receitas')
-    .findOne({ 
-        cod_receita: cod_receita_alvo
-    });
+  const result = await client.db('test').collection('receitas').findOne({
+    cod_receita: cod_receita_alvo,
+  });
 
   if (result) {
     console.log(`Found a prescription in the collection with the cod ${cod_receita_alvo}:`);
@@ -62,68 +55,57 @@ async function findOnePrescriptionByCod(client, cod_receita_alvo) {
 
 // retorna todas as receitas de um determinado paciente
 async function findByPatient(client, patient) {
-  const cursor = client
-    .db('test')
-    .collection('receitas')
-    .find({
-        paciente: patient
-    });
+  const cursor = client.db('test').collection('receitas').find({
+    paciente: patient,
+  });
 
   // Store the results in an array
   const results = await cursor.toArray();
 
   // Print the results
   if (results.length > 0) {
-      console.log(`Found prescription(s) with patient ${patient}:`);
-      results.forEach((result, i) => {
-          const validade = new Date(result.validade).toDateString();
+    console.log(`Found prescription(s) with patient ${patient}:`);
+    results.forEach((result, i) => {
+      const validade = new Date(result.validade).toDateString();
 
-          console.log();
-          console.log(`${i + 1}. medico: ${result.medico}`);
-          console.log(`   _id: ${result._id}`);
-          console.log(`   paciente: ${result.paciente}`);
-          console.log(`   validade: ${validade}`);
-          console.log(`   receituario:`)
-          results.receituario.forEach((medicamento, id) => {
-            console.log(`      medicamento: ${medicamento.medicamento.nome}`);
-            console.log(`      frequencia: ${medicamento.frequencia}`);
-            console.log(`      dose: ${medicamento.dose}`);
-          });
+      console.log();
+      console.log(`${i + 1}. medico: ${result.medico}`);
+      console.log(`   _id: ${result._id}`);
+      console.log(`   paciente: ${result.paciente}`);
+      console.log(`   validade: ${validade}`);
+      console.log(`   receituario:`);
+      results.receituario.forEach((medicamento, id) => {
+        console.log(`      medicamento: ${medicamento.medicamento.nome}`);
+        console.log(`      frequencia: ${medicamento.frequencia}`);
+        console.log(`      dose: ${medicamento.dose}`);
       });
+    });
   } else {
-      console.log(`No prescriptions found with patient ${patient}:`);
+    console.log(`No prescriptions found with patient ${patient}:`);
   }
 }
 
 //atualizar uma determinada consulta
 async function updateAppointmentByCod(client, cod_receita_alvo, newPrescription) {
-  // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
-  const result = await client
-    .db('test')
-    .collection('consultas')
-    .updateOne(
-        { 
-            cod_receita: cod_receita_alvo
-        },
-        {$set: newPrescription});
+  const result = await client.db('test').collection('consultas').updateOne(
+    {
+      cod_receita: cod_receita_alvo,
+    },
+    { $set: newPrescription }
+  );
 
-    if (result) {
-        console.log(`${result.matchedCount} document(s) matched the query criteria.`);
-        console.log(`${result.modifiedCount} document(s) was/were updated.`);;
-    } else {
-        console.log(`No prescription found with the cod ${cod_receita_alvo}:`);
-    }
+  if (result) {
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+  } else {
+    console.log(`No prescription found with the cod ${cod_receita_alvo}:`);
+  }
 }
-
 
 // excluir uma consulta especifica
 async function deleteAppointmentByCod(client, cod_receita_alvo) {
-    const result = await client
-    .db("test")
-    .collection("receitas")
-    .deleteOne({ 
-        cod_receita: cod_receita_alvo
-        });
-    console.log(`${result.deletedCount} document(s) was/were deleted.`);
+  const result = await client.db('test').collection('receitas').deleteOne({
+    cod_receita: cod_receita_alvo,
+  });
+  console.log(`${result.deletedCount} document(s) was/were deleted.`);
 }
-
