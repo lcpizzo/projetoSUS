fbe8c497c63f02f1b7645f7dfe2ac1c73c2dcce6
guijun13 <guilhermejun13.grillo@gmail.com>
2023-06-21 19:12:11 -0300

    merge de novo

--- db/app.js
+++ db/app.js
@@ -2,11 +2,11 @@ import db from './dbConnect.js';
 import express from 'express';
 
 import consultaRoute from './routes/consulta-route.js';
-import funcionarioRoute from './routes/funcionario-route.js';
-import medicamentoRoute from './routes/medicamento-route.js';
-import pacienteRoute from './routes/paciente-route.js';
-import pedidoRoute from './routes/pedido-route.js';
-import postoRoute from './routes/posto-route.js';
+// import funcionarioRoute from './routes/funcionario-route.js';
+// import medicamentoRoute from './routes/medicamento-route.js';
+// import pacienteRoute from './routes/paciente-route.js';
+// import pedidoRoute from './routes/pedido-route.js';
+// import postoRoute from './routes/posto-route.js';
 import receitaRoute from './routes/receita-route.js';
 
 const app = express();
@@ -18,33 +18,16 @@ db.once('open', () => {
   console.log(`Conexão com o banco na porta ${port} feita com sucesso`);
 });
 
-<<<<<<< HEAD
 app.use(express.json());
 
 app.use('/consulta', consultaRoute);
-app.use('/funcionario', funcionarioRoute);
-app.use('/medicamento', medicamentoRoute);
-app.use('/paciente', pacienteRoute);
-app.use('/pedido', pedidoRoute);
-app.use('/posto', postoRoute);
-=======
-// const Consulta = require('./models/consulta.js');
-// const Funcionario = require('./models/funcionario.js');
-// const Medicamento = require('./models/medicamento.js');
-// const Paciente = require('./models/paciente.js');
-// const Pedido = require('./models/pedido.js');
-// const Posto = require('./models/posto.js');
-// const Receita = require('./models/receita.js');
-
-// const consultaRoute = require('./routes/consulta-route.js');
-import consultaRoute from './routes/consulta-route.js';
-import receitaRoute from './routes/receita-route.js';
-// import Consulta from './models/consulta.js';
-
-app.use(express.json());
+// app.use('/funcionario', funcionarioRoute);
+// app.use('/medicamento', medicamentoRoute);
+// app.use('/paciente', pacienteRoute);
+// app.use('/pedido', pedidoRoute);
+// app.use('/posto', postoRoute);
 
 app.use('/consulta', consultaRoute);
->>>>>>> 22eedb85609ddce3ad0e4d497a8e737afaf23552
 app.use('/receita', receitaRoute);
 
 app.listen(port, () => {
--- db/routes/consulta-route.js
+++ db/routes/consulta-route.js
@@ -2,17 +2,12 @@ import express from 'express';
 import consultaControllers from '../controller/consulta.js';
 const router = express.Router();
 
-router.post('/', consultaControllers.post)
-    .get('/', consultaControllers.getAll)
-    .get('/patient', consultaControllers.getByPatient)
-    .get('/tuplaConsulta', consultaControllers.getByTuple)
-    .put('/', consultaControllers.put)
-    .delete('/', consultaControllers.delete);
-
-<<<<<<< HEAD
-export default router;
-=======
-// module.exports = router;
+router
+  .post('/', consultaControllers.post)
+  .get('/', consultaControllers.getAll)
+  .get('/patient', consultaControllers.getByPatient)
+  .get('/tuplaConsulta', consultaControllers.getByTuple)
+  .put('/', consultaControllers.put)
+  .delete('/', consultaControllers.delete);
 
 export default router;
->>>>>>> 22eedb85609ddce3ad0e4d497a8e737afaf23552
